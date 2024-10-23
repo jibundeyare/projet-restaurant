1383e837cba817e92991f22b57eef80358f069d5
jibundeyare <contact@jibundeyare.com>
2024-10-23 11:55:38 +0200

    Ajout de données de test pour les actus

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sql = file_get_contents(__DIR__.'/actus.sql');
+        DB::unprepared($sql);
+
+        $sql = file_get_contents(__DIR__.'/contact.sql');
+        DB::unprepared($sql);
+    }
+}
